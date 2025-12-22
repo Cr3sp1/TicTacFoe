23529457e4de1d3d5130ad2dfa028a73c281f7d6
Cristian <cristiancrespi1@gmail.com>
2025-12-22 14:52:05 +0100

    feat: add enum Mark

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,25 @@
+use std::fmt;
+
 pub fn hello_world() {
-    println!("Hello, world!");
+    println!("Available marks:");
+    println!("X: {}", Mark::X);
+    println!("O: {}", Mark::O);
+    println!("Blank: {}", Mark::Blank);
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Mark {
+    X,
+    O,
+    Blank,
+}
+
+impl fmt::Display for Mark {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Mark::X => write!(f, "X"),
+            Mark::O => write!(f, "O"),
+            Mark::Blank => write!(f, " "),
+        }
+    }
 }
